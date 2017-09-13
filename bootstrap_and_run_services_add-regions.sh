a0a8b898155b489d3f3524d8d4e81b499b033546
thangmv <manvanthang@gmail.com>
2017-09-13 18:02:29 +0700

    update script to add and start 2nd region

---
+++ bootstrap_and_run_services_add-regions.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+cd ./run_vars
+
+#
+#Keystone
+#
+
+docker run -d --name keystone --network=host -u root -v /var/log/apache2:/var/log/apache2 -v /etc/keystone/credential-keys:/etc/keystone/credential-keys -v /etc/keystone/fernet-keys:/etc/keystone/fernet-keys -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./keystone.env" os-controller:4000/keystone
+
+#
+#Glance
+#
+
+printf "\nBootstrap glance..."
+docker run -d --name glance-api --network=host -v /var/log/glance:/var/log/glance -v /var/lib/glance:/var/lib/glance -v /etc/localtime:/etc/localtime -u glance --env-file="./general.env" --env-file="./glance.env" os-controller:4000/glance-api bootstrap.sh
+
+printf "\nWait until bootstrap process done and then start glance containers..."
+RETVAL=1
+
+while [ $RETVAL -eq 1 ]
+do
+	docker ps -a | grep Exit | grep glance-api
+	RETVAL=$?
+done
+
+if [ RETVAL==0 ]
+  then
+	docker ps -a | grep Exit | grep glance-api | cut -d ' ' -f 1 | xargs docker rm -f -v
+
+	docker run -d --name glance-api --network=host -v /var/log/glance:/var/log/glance -v /var/lib/glance:/var/lib/glance -v /etc/localtime:/etc/localtime -u glance --env-file="./general.env" --env-file="./glance.env" os-controller:4000/glance-api
+
+	docker run -d --name glance-registry --network=host -v /var/log/glance:/var/log/glance -v /var/lib/glance:/var/lib/glance -v /etc/localtime:/etc/localtime -u glance --env-file="./general.env" --env-file="./glance.env" os-controller:4000/glance-registry
+
+fi
+
+#
+#Nova
+#
+
+printf "\nBootstrap nova..."
+docker run -d --name nova-api --network=host --privileged -u nova -v /var/log/nova:/var/log/nova -v /var/lib/nova:/var/lib/nova -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./nova.env" os-controller:4000/nova-api bootstrap.sh
+
+printf "\nWait until bootstrap process done and then start nova containers..."
+RETVAL=1
+
+while [ $RETVAL -eq 1 ]
+do
+	docker ps -a | grep Exit | grep nova-api
+	RETVAL=$?
+done
+
+if [ RETVAL==0 ]
+  then
+	docker ps -a | grep Exit | grep nova-api | cut -d ' ' -f 1 | xargs docker rm -f -v
+
+	docker run -d --name nova-api --network=host --privileged -u nova -v /var/log/nova:/var/log/nova -v /var/lib/nova:/var/lib/nova -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./nova.env" os-controller:4000/nova-api
+
+	docker run -d --name nova-conductor --network=host --privileged -u nova -v /var/log/nova:/var/log/nova -v /var/lib/nova:/var/lib/nova -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./nova.env" os-controller:4000/nova-conductor
+
+	docker run -d --name nova-consoleauth --network=host --privileged -u nova -v /var/log/nova:/var/log/nova -v /var/lib/nova:/var/lib/nova -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./nova.env" os-controller:4000/nova-consoleauth
+
+	docker run -d --name nova-novncproxy --network=host --privileged -u nova -v /var/log/nova:/var/log/nova -v /var/lib/nova:/var/lib/nova -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./nova.env" os-controller:4000/nova-novncproxy
+
+	docker run -d --name nova-scheduler --network=host --privileged -u nova -v /var/log/nova:/var/log/nova -v /var/lib/nova:/var/lib/nova -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./nova.env" os-controller:4000/nova-scheduler
+
+	docker run -d --name nova-placement-api --network=host --privileged -u root -v /var/log/apache2:/var/log/apache2 -v /var/lib/nova:/var/lib/nova -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./nova.env" os-controller:4000/nova-placement-api
+
+fi
+
+#
+#Neutron
+#
+
+printf "\nBootstrap neutron..."
+docker run -d --name neutron-server --network=host --privileged --user neutron -v /var/log/neutron:/var/log/neutron -v /var/lib/neutron:/var/lib/neutron -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./neutron.env" os-controller:4000/neutron-server bootstrap.sh
+
+printf "\nWait until bootstrap process done and then start neutron containers..."
+RETVAL=1
+
+while [ $RETVAL -eq 1 ]
+do
+	docker ps -a | grep Exit | grep neutron-server
+	RETVAL=$?
+done
+
+if [ RETVAL==0 ]
+  then
+  	docker ps -a | grep Exit | grep neutron-server | cut -d ' ' -f 1 | xargs docker rm -f -v
+
+	docker run -d --name neutron-server --network=host --privileged --user neutron -v /var/log/neutron:/var/log/neutron -v /var/lib/neutron:/var/lib/neutron -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./neutron.env" os-controller:4000/neutron-server
+
+	docker run -d --name neutron-ovs-agent --network=host --privileged --user neutron -v /var/log/neutron:/var/log/neutron -v /var/lib/neutron:/var/lib/neutron -v /run:/run:shared -v /run/netns:/run/netns:shared -v /run/openvswitch:/run/openvswitch -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./neutron.env" os-controller:4000/neutron-ovs-agent
+
+	docker run -d --name neutron-dhcp-agent --network=host --privileged --user neutron -v /var/log/neutron:/var/log/neutron -v /var/lib/neutron:/var/lib/neutron -v /run:/run:shared -v /run/netns:/run/netns:shared -v /run/openvswitch:/run/openvswitch -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./neutron.env" os-controller:4000/neutron-dhcp-agent
+
+	docker run -d --name neutron-metadata-agent --network=host --privileged --user neutron -v /var/log/neutron:/var/log/neutron -v /var/lib/neutron:/var/lib/neutron -v /run/netns:/run/netns:shared -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./neutron.env" os-controller:4000/neutron-metadata-agent
+
+fi
+
+#
+#Cinder
+#
+
+printf "\nBootstrap cinder..."
+docker run -d --name cinder-api --network=host -u cinder -v /var/log/apache2:/var/log/apache2 -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./cinder.env" os-controller:4000/cinder-api bootstrap.sh
+
+printf "\nWait until bootstrap process done and then start cinder containers..."
+RETVAL=1
+
+while [ $RETVAL -eq 1 ]
+do
+	docker ps -a | grep Exit | grep cinder-api
+	RETVAL=$?
+done
+
+if [ RETVAL==0 ]
+  then
+  	docker ps -a | grep Exit | grep cinder-api | cut -d ' ' -f 1 | xargs docker rm -f -v
+
+	docker run -d --name cinder-api --network=host -u root -v /var/log/apache2:/var/log/apache2 -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./cinder.env" os-controller:4000/cinder-api
+
+	docker run -d --name cinder-scheduler --network=host -u cinder -v /var/log/cinder:/var/log/cinder -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./cinder.env" os-controller:4000/cinder-scheduler
+
+	docker run -d --name cinder-volume --network=host -u cinder -v /var/log/cinder:/var/log/cinder -v /etc/localtime:/etc/localtime --env-file="./general.env" --env-file="./cinder.env" os-controller:4000/cinder-volume
+
+fi
+
+printf "\nFinishing to run containers for OpenStack services!!!"
+
--- create_region.sh
+++ create_region.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+#Region
 openstack --os-region-name $1 endpoint create --region $2 identity admin http://$3:35357/v3/
 openstack --os-region-name $1 endpoint create --region $2 identity public http://$3:5000/v3/
 openstack --os-region-name $1 endpoint create --region $2 identity internal http://$3:5000/v3/
+
+#Glance
+openstack --os-region-name $1 endpoint create --region $2 image public http://$3:9292
+openstack --os-region-name $1 endpoint create --region $2 image internal http://$3:9292
+openstack --os-region-name $1 endpoint create --region $2 image admin http://$3:9292
+
+#Nova
+openstack --os-region-name $1 endpoint create --region $2 compute public http://$3:8774/v2.1
+openstack --os-region-name $1 endpoint create --region $2 compute internal http://$3:8774/v2.1
+openstack --os-region-name $1 endpoint create --region $2 compute admin http://$3:8774/v2.1
+openstack --os-region-name $1 endpoint create --region $2 placement public http://$3:8778
+openstack --os-region-name $1 endpoint create --region $2 placement internal http://$3:8778
+openstack --os-region-name $1 endpoint create --region $2 placement admin http://$3:8778
+
+#Neutron
+openstack --os-region-name $1 endpoint create --region $2 network public http://$3:9696
+openstack --os-region-name $1 endpoint create --region $2 network internal http://$3:9696
+openstack --os-region-name $1 endpoint create --region $2 network admin http://$3:9696
+
+#Cinder
+openstack --os-region-name $1 endpoint create --region $2 volumev2 public http://$3:8776/v2/%\(project_id\)s
+openstack --os-region-name $1 endpoint create --region $2 volumev2 internal http://$3:8776/v2/%\(project_id\)s
+openstack --os-region-name $1 endpoint create --region $2 volumev2 admin http://$3:8776/v2/%\(project_id\)s
+openstack --os-region-name $1 endpoint create --region $2 volumev3 public http://$3:8776/v3/%\(project_id\)s
+openstack --os-region-name $1 endpoint create --region $2 volumev3 internal http://$3:8776/v3/%\(project_id\)s
+openstack --os-region-name $1 endpoint create --region $2 volumev3 admin http://$3:8776/v3/%\(project_id\)s
